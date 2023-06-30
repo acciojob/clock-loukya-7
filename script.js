49bf117d30293e675f9cef6ca77fd8b9f556a399
loukya-7 <131159563+loukya-7@users.noreply.github.com>
2023-06-30 21:12:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function updateTime() {
+  const timer = document.getElementById('timer');
+  const currentDate = new Date();
+  const currentTime = currentDate.toLocaleTimeString();
+  const currentDateFormatted = currentDate.toLocaleDateString();
+  timer.textContent = `${currentDateFormatted} ${currentTime}`;
+}
+
+setInterval(updateTime, 1000);
+
